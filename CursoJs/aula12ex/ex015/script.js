9ccecdf31560d82b17d2841e898179919a60e370
Catarina Dalsan Escolar Ribeiro <125068498+Catyz00@users.noreply.github.com>
2024-05-13 16:48:45 -0300

    add fotos e aprendi estrutura de repeticoes

--- CursoJs/aula12ex/ex015/script.js
+++ CursoJs/aula12ex/ex015/script.js
@@ -10,13 +10,41 @@ function verificar() {
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fano.value);
         var genero = '';
-
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
         if (fsex[0].checked) {
             genero = 'homem';
+            if (idade >=0 && idade < 10){
+                // crianca
+                img.setAttribute('src', 'foto-criancaH.png');
+            }else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'foto-jovemH.png');
+            }else if (idade < 60){
+                //adulto
+                img.setAttribute('src', 'foto_adultoH.png');
+            }else { 
+                //idoso 
+                img.setAttribute('src', 'foto-idoso.png');
+            }
         } else if (fsex[1].checked) {
             genero = 'mulher';
+            if (idade >=0 && idade < 10){
+                // crianca
+                img.setAttribute('scr', 'foto_criancaM.png');
+            }else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'foto-jovemM.png');
+            }else if (idade < 60){
+                //adulto
+                img.setAttribute('src', 'foto-adutoM.png');
+            }else { 
+                //idoso 
+                img.setAttribute('src', 'foto_idosa.png');
+            }
         }
         res.style.textAlign = 'center';
         res.innerHTML = (`Detectamos ${genero} com ${idade} anos.`);
+        res.appendChild(img);
     }
 }
